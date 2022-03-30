e2ed45ab35d363696e218f88a0f708a221897852
Makingagame <samuelwillems82@gmail.com>
2022-03-30 23:05:16 +1030

    Slider and Radio Buttons and some watchlist HTML

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-console.log('Hello World!');
-var apikey = "9f8a2da0"
-
+const apikey = "9f8a2da0"
 const searchMovie = document.getElementById("searchMovie");
 
+//Year Range Slider Function
+$(function() {
+    $( "#slider-3" ).slider({
+       range:true,
+       min: 1900,
+       max: 2040,
+       values: [ 1970, 2015 ],
+       slide: function( event, ui ) {
+          $( "#year" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+       }
+    });
+    $( "#year" ).val( $( "#slider-3" ).slider( "values", 0 ) +
+       " - " + $( "#slider-3" ).slider( "values", 1 ) );
+ });
 
-//On keyup (user releases keyboard key) run below (send an API call and retrieve results)
+
+//On keyup (user releases keyboard key) run below (send an API call and retrieve results based on filters)
 searchMovie.addEventListener("keyup", e => { 
-    const searchMovieString = e.target.value; 
-
-    console.log(searchMovieString);
-
-
-$(document).ready(function(){
-
-    console.log(apikey);
-
-        var searchMovie = $("#searchMovie").val()
-
-        var result = ""
-
-        var url = "http://www.omdbapi.com/?apikey="+apikey
+    const searchMovieString = e.target.value;
+    $(document).ready(function(){
+        let searchMovie = $("#searchMovie").val()
+        let result = ""
+        let url = "http://www.omdbapi.com/?apikey="+apikey
 
         $.ajax({
             method:'GET',
@@ -50,6 +54,6 @@ $(document).ready(function(){
             }
         })
 
-})
+    })
 });
   
